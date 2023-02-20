d558735a48014d70b1f8b4e33fd5651fb8740288
minjian.c <448900450@qq.com>
2023-02-20 12:32:07 +0800

    修复Android Q 系统导致的已知 IRequestFinishCallback stub 内存泄露

--- lib_base/src/main/java/com/cmj/wanandroid/lib/base/BaseActivity.kt
+++ lib_base/src/main/java/com/cmj/wanandroid/lib/base/BaseActivity.kt
@@ -1,9 +1,12 @@
 package com.cmj.wanandroid.lib.base
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.OnBackPressedDispatcher
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -17,9 +20,26 @@ import pub.devrel.easypermissions.EasyPermissions
 
 abstract class BaseActivity<VM : ViewModel, VB : ViewBinding> : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
+    companion object {
+        @JvmStatic
+        val fallbackOnBackPressedField by lazy {
+            if (Build.VERSION.SDK_INT != Build.VERSION_CODES.Q) {
+                null
+            } else {
+                // OnBackPressedDispatcher 中只有fallbackOnBackPressed是Runnable
+                // 这样可以避免代码混淆带来的影响
+                OnBackPressedDispatcher::class.java.declaredFields.find {
+                    it.type.isAssignableFrom(Runnable::class.java)
+                }
+            }
+        }
+    }
+
     init {
         LogMan.i("BaseActivity", "${this::class.java.simpleName} init")
     }
+
+    private var fallbackOnBackPressed: Runnable? = null
 
     protected val viewModel by genericViewModels<VM> { provideViewModelFactory() ?: defaultViewModelProviderFactory }
     protected val binding by genericBinding<VB>()
@@ -37,6 +57,7 @@ abstract class BaseActivity<VM : ViewModel, VB : ViewBinding> : AppCompatActivit
             setContentView(view)
             onViewCreated(view, savedInstanceState)
         }
+        fixAndroidQMemoryLeak()
     }
 
     protected open fun onCreateView(inflater: LayoutInflater, savedInstanceState: Bundle?): View? {
@@ -98,5 +119,40 @@ abstract class BaseActivity<VM : ViewModel, VB : ViewBinding> : AppCompatActivit
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+
+    private fun fixAndroidQMemoryLeak() {
+        if (Build.VERSION.SDK_INT != Build.VERSION_CODES.Q) return
+        fallbackOnBackPressedField?.runCatching {
+            isAccessible = true
+            // 缓存默认的 fallbackOnBackPressed，如果不是TaskRoot还可以直接用它
+            fallbackOnBackPressed = get(onBackPressedDispatcher) as? Runnable
+            if (fallbackOnBackPressed != null) {
+                // 替换默认的 fallbackOnBackPressed
+                set(onBackPressedDispatcher, Runnable {
+                    onFallbackOnBackPressed()
+                })
+            }
+            fallbackOnBackPressedField?.isAccessible = false
+        }?.onFailure {
+            fallbackOnBackPressedField?.isAccessible = false
+        }
+    }
+
+    /**
+     * FragmentActivity等都是利用[mOnBackPressedDispatcher]addCallback完成Fragment pop，
+     * 并且会优先于[OnBackPressedDispatcher]#mFallbackOnBackPressed执行
+     */
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun onFallbackOnBackPressed() {
+        // 如果不是TaskRoot，不存在内存泄漏，执行原有函数就可以
+        if (!isTaskRoot) {
+            fallbackOnBackPressed?.run()
+            return
+        }
+        // actionBar#collapseActionView 属于私有函数，所以就不要在Activity中使用[android.app.ActionBar]
+        // if (actionBar != null && actionBar.collapseActionView()) return
+        if (!fragmentManager.isStateSaved && fragmentManager.popBackStackImmediate()) return
+        finishAfterTransition()
     }
 }
