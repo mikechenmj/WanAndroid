30a78dd69622e913fc2d5b66fdbbbbacba2194f4
minjian.c <448900450@qq.com>
2023-02-20 15:14:52 +0800

    整理 app icon

--- app/src/main/java/com/cmj/wanandroid/start/StartActivity.kt
+++ app/src/main/java/com/cmj/wanandroid/start/StartActivity.kt
@@ -8,15 +8,16 @@ import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
-import com.cmj.wanandroid.lib.base.BaseActivity
 import com.cmj.wanandroid.content.MainActivity
+import com.cmj.wanandroid.databinding.LayoutStartBinding
+import com.cmj.wanandroid.lib.base.BaseActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class StartActivity : BaseActivity<ViewModel, ViewBinding>() {
+class StartActivity : BaseActivity<ViewModel, LayoutStartBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launch {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
@@ -26,11 +27,6 @@ class StartActivity : BaseActivity<ViewModel, ViewBinding>() {
             finish()
         }
     }
-
-    override fun onCreateView(inflater: LayoutInflater, savedInstanceState: Bundle?): View? {
-        return null
-    }
-
     override fun finish() {
         super.finish()
         overridePendingTransition(0, 0)
